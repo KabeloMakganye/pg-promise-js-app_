eba01ff7c80a1ea569c5456c0db7b023a0ad2175
KabeloMakganye <programming@eafricatelecoms.co.za>
2021-08-16 12:11:47 +0200

    Added Procfile2

--- app.js
+++ app.js
@@ -42,7 +42,14 @@ class ad{
 }
 z=new ad();
 
-app.use(cors());
+app.use(cors())
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
+
 app.get('/in/:name/:email',(req,res,next)=> {
     db.func("fn_add_new_clock",[req.params.email,req.params.name])
     // db.any("SELECT * FROM clock_in_out")
