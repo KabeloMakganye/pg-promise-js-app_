7889719aedae0052fc8ea77acf63a5bc55055916
KabeloMakganye <programming@eafricatelecoms.co.za>
2021-10-13 16:27:45 +0200

    Added Procfile2

--- app.js
+++ app.js
@@ -240,6 +240,17 @@ app.get('/get_pir/',(req,res)=> {
      })
 })
 
+app.get('/fn_get_gitlinks/',(req,res)=> {
+    db.func("fn_get_gitlinks")
+     .then(rows => {
+         console.log(rows);
+         res.json(rows);
+     })
+     .catch(error => {
+         console.log(error);
+     })
+})
+
 /* http.createServer(function(request, response) {
     response.writeHead(200, {"Constent-Type": "test/plain"})
     response.end("kabelo TESTING\n")
