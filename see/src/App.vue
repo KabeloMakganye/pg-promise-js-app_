d3bdd95908bcc14ab4e1a785fe6ce394d329e9c2
KabeloMakganye <programming@eafricatelecoms.co.za>
2022-01-19 18:16:02 +0200

    Added Procfile3

--- see/src/App.vue
+++ see/src/App.vue
@@ -8,7 +8,7 @@
          </div>
          <div class="navin2">
         <i class="fa fa-phone" aria-hidden="true"></i> <a href="tel:0121117420"><span class="num"> 012 111 7420</span></a>
-        <i class="fa fa-phone" aria-hidden="true"></i> <a href="tel:0876546448"><span class="num"> 087 654 6448</span> </a>
+        <i class="fa fa-phone" id="b" aria-hidden="true"></i> <a href="tel:0876546448"><span class="num"> 087 654 6448</span> </a>
         <i class="fa fa-envelope" aria-hidden="true"></i> <a href="mailto:info@eafricatelecoms.co.za"><span class="num"> info@eafricatelecoms.co.za</span></a>
          </div>
       </div>
@@ -155,6 +155,9 @@ img {
     font-size: 25px;
     margin: 0;
     padding: 0px 0px 0px 15px;
+  }
+  #b {
+    display: none;
   }
   a .num {
     display: none;
